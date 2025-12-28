9a8afff2c76a10555540cc78388cc31e4e204a45
Ashutosh <ashutosh10teotia@gmail.com>
2025-12-28 23:33:45 +0530

    using components properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
-import Products from './Products'
+import React from 'react'
+import Products from './components/Products.jsx'
 
 function App() {
-  const [a,b] = useState(true);
   return (
-    <div className="w-full h-screen bg-zinc-700">
-      <button className='px-3 py-1 bg-green-500 rounded-md text-xs' onClick={()=>b(!a)}>click</button>
+    <div>
+    <Products></Products>
     </div>  
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-
-function Products({a}) {
+import React, { useState } from 'react'
+function Products() {
+  const [a,b] = useState(true);
   return (
     <div className='text-white h-90 bg-zinc-900'>
+      <button className='px-3 py-1 bg-green-500 rounded-md text-xs' onClick={()=>b(!a)}>click</button>
         <h1 className= {`${a === false? "text-red-500" : "text-blue-400"}`}>
           {a === false? "hey" : "hello"}</h1>
     </div>
